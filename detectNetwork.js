f49d0f842daa65774b473c88f30182fc7bde3f31
Jessica_Wildermuth <jwildermuth536@gmail.com>
2020-06-23 18:31:43 -0700

    Add Visa and MasterCard Networks

--- detectNetwork.js
+++ detectNetwork.js
@@ -10,18 +10,40 @@
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+  
+  var network = "Card network not recognized";
 
-  var length = cardNumber.length;
- 
-  if (length !== 14 || length !== 15 || cardNumber[0] !== '3') {
-    return "Card Network Not Recognized";
-  } else if ((cardNumber[1] === '8' || cardNumber[1] === '9')  && length === 14) {
-      return "Diner's Club";
-  } else if ((cardNumber[1] === '4' || cardNumber[1] === '7') && length === 15) {
-      return "American Express";
-  } else {
-      return "Card Network Not Recognized";
-  }
+isValidLength = false;
+if (cardNumber.length === 14 || cardNumber.length === 15 || cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19) {
+  isValidLength = true;
+}
+
+if (isValidLength === false) {
+  return network;
+}
+
+if (cardNumber[0] !== '3'|| cardNumber[0] !== '4' || cardNumber[0] !== '5') {
+  return network;
+}
+
+
+if ((cardNumber[1] === '8' || cardNumber[1] === '9') && cardNumber[0] === '3') {
+  network = "Diner's Club";
+}
+
+if ((cardNumber[1] === '4' || cardNumber[1] === '7') && cardNumber[0] === '3') {
+  network = "American Express";
+}
+
+if (cardNumber[0] === '4') {
+  network = 'Visa';
+}
+
+if (cardNumber[0] === '5' && (cardNumber[1] === '1' || cardNumber[1] === '2' || cardNumber[1] === '3' || cardNumber[1] === '4' || cardNumber[1] === '5')) {
+  network = 'MasterCard';
+}
+
+return network;
  
  // The American Express network always starts with a 34 or 37 and is 15 digits long
 
